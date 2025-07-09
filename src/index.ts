216a637ee39efd977e1471c8b1135b5d6e63e561
Tim Goyer <tim.goyer@gmail.com>
2025-07-08 21:55:01 -0500

    Revert to RGB and add OkLab seperate

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export * from "./dark";
-export * from "./light";
+export * from './dark';
+export * from './dark.oklab';
+
+export * from './light';
+export * from './light.oklab';
--- src/preview/colors.stories.tsx
+++ src/preview/colors.stories.tsx
@@ -1,18 +1,20 @@
-import { dark, light } from '../';
+import { dark, darkOkLab, light, lightOkLab } from '../';
 
 import { Theme } from '../types/Color';
 import { ThemeViewer, ThemeViewerProps } from './components/ThemeViewer';
 
 const themes: Record<string, Theme> = {
     dark: dark,
+    darkOkLab: darkOkLab,
     light: light,
+    lightOkLab: lightOkLab,
 };
 
 const meta = {
     title: 'Themes',
     argTypes: {
         themeName: {
-            options: ['dark', 'light'],
+            options: ['dark', 'darkOkLab', 'light', 'lightOkLab'],
             control: { type: 'radio' },
         },
     },
