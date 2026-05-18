a08fe14ffb2596d6fc22fac5d6a10ce14a454cc8
Tim Goyer <tim.goyer@gmail.com>
2026-05-18 18:08:43 -0500

    Format cleanup

--- src/palette.test.ts
+++ src/palette.test.ts
@@ -10,7 +10,9 @@ describe('palette', () => {
 
   it('has twelve steps per named scale', () => {
     for (const name of Object.keys(palette)) {
-      const steps = Object.keys(palette[name]).map(Number).sort((a, b) => a - b);
+      const steps = Object.keys(palette[name])
+        .map(Number)
+        .sort((a, b) => a - b);
       expect(steps).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
     }
   });
--- src/preview/utility.ts
+++ src/preview/utility.ts
@@ -1,18 +1,17 @@
-
 /** @param {string} paletteName @param {number} step */
 export function cssCustomPropertyName(paletteName: string, step: number) {
-    return `--${toCssCasing(paletteName)}${step}`;
+  return `--${toCssCasing(paletteName)}${step}`;
 }
 
 /** @param {string} paletteName @param {number} step */
 export function cssVarRef(paletteName: string, step: number) {
-    return `var(--${toCssCasing(paletteName)}${step})`;
+  return `var(--${toCssCasing(paletteName)}${step})`;
 }
 
 /** @param {string} str */
 export function toCssCasing(str: string): string {
-    return str
-        .replace(/([a-z])(\d)/, '$1-$2')
-        .replace(/([A-Z])/g, '-$1')
-        .toLowerCase();
+  return str
+    .replace(/([a-z])(\d)/, '$1-$2')
+    .replace(/([A-Z])/g, '-$1')
+    .toLowerCase();
 }
